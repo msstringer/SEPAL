9b2a92fe1cc118d7265924ed3c343dcfadb4fffd
msstringer <36669569+msstringer@users.noreply.github.com>
2024-01-30 13:42:49 +0000

    Create setup.py
    Add for easier installation through pip

---
+++ setup.py
@@ -0,0 +1,35 @@
+# Authors: Based on https://github.com/ucl-pond/pySuStaIn/blob/master/setup.py 
+# License: TBC
+__version__ = '0.1'
+
+from setuptools import setup
+
+#parse the requirement.txt file, ignoring commented lines, placing results in install_reqs
+with open('requirements.txt', 'r') as f:
+    install_reqs = [
+        s for s in [
+            line.strip(' \n') for line in f
+        ] if not s.startswith('#') and s != ''
+    ]
+
+
+print("Started SEPAL setup.py")
+
+setup(name=               'SEPAL',
+      version=            __version__,
+      description=        'SEPAL',
+      url=                'https://github.com/mjt320/SEPAL',
+      classifiers=			  ['Intended Audience :: Science/Research',
+                   			  'Programming Language :: Python',
+                   			  'Topic :: Scientific/Engineering',
+                   			  'Programming Language :: Python :: 3.7'],
+      maintainer=         '',
+      maintainer_email=   '',
+      license=		        'TBC',
+      packages=			      ['SEPAL'],
+      python_requires=  	'>=3.7',
+      install_requires =  install_reqs,	#the parsed requirements from requirements.txt
+      entry_points=			  {},
+      zip_safe=				    False)
+
+print("Finished SEPAL setup.py")
