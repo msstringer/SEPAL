fbb8b337dbb737ac6e1d656c18e432b6b6718c25
msstringer <36669569+msstringer@users.noreply.github.com>
2024-01-30 14:39:51 +0000

    update fitting.py to SEPAL_utils

--- SEPAL/fitting.py
+++ SEPAL/fitting.py
@@ -16,7 +16,7 @@ import nibabel as nib
 import numpy as np
 from joblib import Parallel, delayed
 
-from utils.imaging import read_images, write_image
+from SEPAL_utils.imaging import read_images, write_image
 
 
 class Fitter(ABC):
